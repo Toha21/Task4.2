3c7c1c115043442e961d2812568168b2e8636b13
Anton Gavryukhin <ah21@mail.ru>
2022-07-09 21:14:16 +0300

    Modified commit

---
+++ src/BodyMassIndexService.java
@@ -0,0 +1,9 @@
+public class BodyMassIndexService {
+
+    public double calculate(double weight, double height) {
+
+        double heightSquared = height * height; // рост в метрах квадратных
+        double calculate = weight / heightSquared;
+        return calculate;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
-        public static void main(String[] args) {
-            BmiService service = new BmiService();
-            double weight = 95;//вес
-            double height = 1.72;//рос в метрах
-            double massIndex = service.calculate(weight,height);
-            System.out.println("Индекс массы тула :" + massIndex);
-        }
+    public static void main (String[] args) {
+        BodyMassIndexService service = new BodyMassIndexService();
+
+        double weight = 95;//масса тела
+        double height = 1.72;//рос в метрах
+        double massIndex = service.calculate(weight, height);
+
+        System.out.println("Индекс массы туле: " + massIndex);
     }
+}
 
